80ca8600a2316b9ecc4e4f645287267b9c1323a5
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-29 03:06:53 +0300

    Added extract.cmd.

---
+++ extract.cmd
@@ -0,0 +1,14 @@
+@"%~dp0tools\pour_wrapper_windows.exe" --script "%0" %* && exit /B 0 || exit /B 1
+
+pour.chdir(SCRIPT_DIR)
+
+ext2read.exclude(
+    "/etc/Xconfig",
+    "/root/.mc/history",
+    "/root/.bash_history",
+    "/tmp/.X11-unix/X0",
+    "/tmp/chime.mp3",
+    "/var/X11R6/.skip"
+    )
+
+ext2read.dump_vhd(".disk.vhd", "disk_root")
